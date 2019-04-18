acffd7f9d990be8042e57cb88d9b1700ba39d45f
Chris de Graaf <chrisadegraaf@gmail.com>
2019-04-18 12:53:38 -0500

    Refactor for testability, add tests

--- github/main.go
+++ github/main.go
@@ -21,12 +21,18 @@ var (
 	registratorUsername = os.Getenv("REGISTRATOR_USERNAME")
 	registryBranch      = os.Getenv("REGISTRY_BRANCH")
 	webhookSecret       = []byte(os.Getenv("GITHUB_WEBHOOK_SECRET"))
-	repoRegex           = regexp.MustCompile(`Repository:\s+(.*)`)
-	repoPiecesRegex     = regexp.MustCompile(`github.com/(.*)/(.*)`)
-	versionRegex        = regexp.MustCompile(`Version:\s+(v.*)`)
-	commitRegex         = regexp.MustCompile(`Commit:\s+(.*)`)
+	repoRegex           = regexp.MustCompile(`Repository:.*github.com/(.*)/(.*)`)
+	versionRegex        = regexp.MustCompile(`Version:\s*(v.*)`)
+	commitRegex         = regexp.MustCompile(`Commit:\s*(.*)`)
 	ctx                 = context.Background()
 	client              *github.Client
+
+	ErrNotMergeEvent  = errors.New("Not a merge event")
+	ErrNotRegistrator = errors.New("PR not created by Registrator")
+	ErrBaseBranch     = errors.New("Base branch is not the default")
+	ErrRepoMatch      = errors.New("No repo regex match")
+	ErrVersionMatch   = errors.New("No version regex match")
+	ErrCommitMatch    = errors.New("No commit regex match")
 )
 
 // LambdaRequest is what we get from AWS Lambda.
@@ -36,7 +42,16 @@ type LambdaRequest struct {
 	Body    string            `json:"body"`
 }
 
+// Reponse is what we return from the handler.
 type Response events.APIGatewayProxyResponse
+
+// ReleaseInfo contains the information needed to create a GitHub release.
+type ReleaseInfo struct {
+	Owner   string
+	Name    string
+	Version string
+	Commit  string
+}
 
 func init() {
 	appID, err := strconv.Atoi(os.Getenv("GITHUB_APP_ID"))
@@ -68,40 +83,49 @@ func main() {
 	}
 
 	lambda.Start(func(lr LambdaRequest) (response Response, nilErr error) {
-		// It doesn't matter what we return to the webhook.
-		response = Response{StatusCode: 204}
+		response = Response{StatusCode: 200}
+		defer func(r *Response) {
+			if r.Body != "" {
+				fmt.Println(r.Body)
+			} else {
+				fmt.Println("No error")
+			}
+		}(&response)
 
-		// Convert the request to an HTTP request.
-		r, err := lambdaToHttp(lr)
+		r, err := LambdaToHttp(lr)
 		if err != nil {
-			fmt.Println("Converting request:", err)
+			response.Body = "Converting request: " + err.Error()
 			return
 		}
 
-		// Validate the payload.
 		payload, err := github.ValidatePayload(r, webhookSecret)
 		if err != nil {
-			fmt.Println("Validating payload:", err)
+			response.Body = "Validating payload: " + err.Error()
 			return
 		}
 
-		// Parse the event.
 		event, err := github.ParseWebHook(github.WebHookType(r), payload)
 		if err != nil {
-			fmt.Println("Parsing payload:", err)
+			response.Body = "Parsing payload: " + err.Error()
 			return
 		}
 
-		// Check the event type.
 		pre, ok := event.(*github.PullRequestEvent)
 		if !ok {
-			fmt.Println("Unknown event type:", github.WebHookType(r))
+			response.Body = "Unknown event type: " + github.WebHookType(r)
 			return
 		}
 
-		// Handle the pull request event.
-		if err = handlePullRequestEvent(pre); err != nil {
-			fmt.Println("Event handling:", err)
+		PrintInfo(pre)
+
+		if err = ShouldRelease(pre); err != nil {
+			response.Body = "Validation: " + err.Error()
+			return
+		}
+
+		ri := ParseBody(PreprocessBody(pre.GetPullRequest().GetBody()))
+		if err := ri.DoRelease(); err != nil {
+			response.Body = "Creating release: " + err.Error()
 			return
 		}
 
@@ -109,8 +133,8 @@ func main() {
 	})
 }
 
-// lambdaToHttp converts a Lambda request to an HTTP request.
-func lambdaToHttp(lr LambdaRequest) (*http.Request, error) {
+// LambdaToHttp converts a Lambda request to an HTTP request.
+func LambdaToHttp(lr LambdaRequest) (*http.Request, error) {
 	r, err := http.NewRequest(lr.Method, "", bytes.NewBufferString(lr.Body))
 	if err != nil {
 		return nil, err
@@ -121,8 +145,8 @@ func lambdaToHttp(lr LambdaRequest) (*http.Request, error) {
 	return r, nil
 }
 
-// handlePullRequest creates a release from a merged pull request.
-func handlePullRequestEvent(pre *github.PullRequestEvent) error {
+// PrintInfo prints out some logging information.
+func PrintInfo(pre *github.PullRequestEvent) {
 	pr := pre.GetPullRequest()
 	u := pr.GetUser()
 	body := strings.TrimSpace(pr.GetBody())
@@ -153,61 +177,74 @@ PR Body:
 		pr.GetTitle(),
 		body,
 	)
+}
 
-	// Check that the event is a PR merge.
+// PreprocessBody preprocesses the PR body.
+func PreprocessBody(body string) string {
+	return strings.TrimSpace(strings.Replace(body, "\r\n", "\n", -1))
+}
+
+// ShouldRelease determines whether the PR indicates a release.
+func ShouldRelease(pre *github.PullRequestEvent) error {
+	pr := pre.GetPullRequest()
+	u := pr.GetUser()
+	body := pr.GetBody()
+
 	if pre.GetAction() != "closed" || !pr.GetMerged() {
-		return errors.New("Not a merge event")
+		return ErrNotMergeEvent
 	}
 
-	// Check that the PR creator is Registrator.
 	if u.GetLogin() != registratorUsername {
-		return errors.New("PR not created by Registrator")
+		return ErrNotRegistrator
 	}
 
-	// Check that the base branch is the default.
 	if pr.GetBase().GetRef() != registryBranch {
-		return errors.New("Base branch is not the default")
+		return ErrBaseBranch
 	}
 
-	// Get the repository URL.
+	if !repoRegex.MatchString(body) {
+		return ErrRepoMatch
+	}
+
+	if !versionRegex.MatchString(body) {
+		return ErrVersionMatch
+	}
+
+	if !commitRegex.MatchString(body) {
+		return ErrCommitMatch
+	}
+
+	return nil
+}
+
+// ParseBody parses the PR body and returns a ReleaseInfo.
+// The assumption is that the PR body is valid.
+func ParseBody(body string) ReleaseInfo {
 	match := repoRegex.FindStringSubmatch(body)
-	if match == nil {
-		return errors.New("No repo regex match")
-	}
-	repoURL := match[1]
-	fmt.Println("Extracted repo URL:", repoURL)
-
-	// Get the repository owner and name.
-	match = repoPiecesRegex.FindStringSubmatch(repoURL)
-	if match == nil {
-		return errors.New("No repo pieces regex match")
-	}
 	owner, name := match[1], match[2]
-	fmt.Println("Extracted repo owner:", owner)
-	fmt.Println("Extracted repo name:", name)
 
-	// Get the package version.
 	match = versionRegex.FindStringSubmatch(body)
-	if match == nil {
-		return errors.New("No version regex match")
-	}
 	version := match[1]
-	fmt.Println("Extracted package version:", version)
 
-	// Get the release commit hash.
 	match = commitRegex.FindStringSubmatch(body)
-	if match == nil {
-		return errors.New("No commit regex match")
-	}
 	commit := match[1]
-	fmt.Println("Extracted commit:", commit)
 
-	// Create the release.
-	release := &github.RepositoryRelease{TagName: &version, TargetCommitish: &commit}
-	if _, _, err := client.Repositories.CreateRelease(ctx, owner, name, release); err != nil {
+	return ReleaseInfo{
+		Owner:   owner,
+		Name:    name,
+		Version: version,
+		Commit:  commit,
+	}
+}
+
+// DoRelease creates the GitHub release.
+func (ri ReleaseInfo) DoRelease() error {
+	r := github.RepositoryRelease{TagName: &ri.Version, TargetCommitish: &ri.Commit}
+
+	if _, _, err := client.Repositories.CreateRelease(ctx, ri.Owner, ri.Name, &r); err != nil {
 		return errors.New("Creating release: " + err.Error())
 	}
 
-	fmt.Printf("Created release %s for %s/%s at %s\n", version, owner, name, commit)
+	fmt.Printf("Created release %s for %s/%s at %s\n", ri.Version, ri.Owner, ri.Name, ri.Commit)
 	return nil
 }
