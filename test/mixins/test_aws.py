5186d561b53973c1ae468be6e9671397ef735362
Chris de Graaf <chrisadegraaf@gmail.com>
2019-06-21 17:08:36 -0500

    Test AWS mixin

---
+++ test/mixins/test_aws.py
@@ -0,0 +1,44 @@
+import json
+
+from unittest.mock import Mock
+
+from tagbot import stages
+from tagbot.context import Context
+from tagbot.mixins import AWS
+
+mixin = AWS()
+mixin._function_prefix = "prefix-"
+mixin._lambda.invoke = Mock()
+
+def test_invoke():
+    mixin.invoke("fun", {"foo": "bar"})
+    mixin._lambda.invoke.assert_called_once_with(
+        FunctionName="prefix-fun", InvocationType="Event", Payload='{"foo": "bar"}'
+    )
+    mixin._lambda.invoke.reset_mock()
+
+
+def test_invoke_notify():
+    ctx = Context(
+        registry="foo/bar",
+        repo="bar/foo",
+        version="v0.1.2",
+        commit="abc",
+        issue=1,
+        id="id",
+        auth="token",
+    )
+    copied = Context(**ctx.__dict__.copy())
+    copied.notification = "notification"
+    mixin.invoke_notify(ctx, "notification")
+    mixin._lambda.invoke.assert_called_once_with(
+        FunctionName=f"prefix-{stages.notify}",
+        InvocationType="Event",
+        Payload=json.dumps(copied.__dict__),
+    )
+    # TODO: This is kind of an undesired side effect.
+    assert ctx.notification == "notification"
+
+
+def test_function():
+    assert mixin._function("foo") == "prefix-foo"
