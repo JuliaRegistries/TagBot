354b7a7b1cd195b923efa1831336c3e1fbf55d86
Chris de Graaf <chrisadegraaf@gmail.com>
2019-11-03 20:33:17 +0700

    Support GPG signing

--- tagbot/__main__.py
+++ tagbot/__main__.py
@@ -2,12 +2,14 @@ from . import *
 
 versions = get_new_versions()
 for version, tree in versions:
+    info(f"Processing version: {version}")
     sha = commit_from_tree(tree)
     if not sha:
         warn(f"Version {version} doesn't seem to have a matching commit")
         continue
     if release_exists(version):
-        die(warn, f"Release {version} already exists", 0)
+        warn(f"Release {version} already exists")
+        continue
     create_tag(version, sha)
     log = get_changelog(version)
     create_release(version, sha, log)
