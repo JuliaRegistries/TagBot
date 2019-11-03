be3d3288efb7c6137a2f40013051099652e7204e
Chris de Graaf <chrisadegraaf@gmail.com>
2019-11-03 10:34:20 +0700

    Work on cron jobs instead of explicit triggers

--- tagbot/__init__.py
+++ tagbot/__init__.py
@@ -1,8 +1,8 @@
-import json
 import os
 import subprocess
+import tempfile
 
-from typing import Any, Callable, Optional
+from typing import Any, Dict, Callable, List, Optional
 
 import toml
 
@@ -27,29 +27,14 @@ def die(level: Callable[[Any], None], msg: str, status: int) -> None:
     exit(status)
 
 
-def read_event(path: str) -> dict:
-    with open(path) as f:
-        return json.load(f)
-
-
-def validate(data: dict) -> None:
-    if env.EVENT_NAME != "issue_comment":
-        die(info, "Not an issue comment", 0)
-    if data["action"] != "created":
-        die(info, "Not a new issue comment", 0)
-    # TODO: Put me back when not testing myself
-    # if data["sender"]["login"] != "github-actions[bot]":
-    #     die(info, f"Comment not by GitHub Actions", 0)
-    if not env.TOKEN:
-        die(error, "The 'token' input parameter is required", 1)
-
-
-parse_body = toml.loads
-
-
-def git(*argv: str) -> str:
-    """Run a Git command in the repository."""
-    args = ["git", "-C", env.REPO_DIR, *argv]
+def git(*argv: str, root=env.REPO_DIR) -> str:
+    """Run a Git command."""
+    args = ["git"]
+    if root:
+        args.extend(["-C", root])
+    args.extend(argv)
+    cmd = " ".join(args)
+    debug(f"Running {cmd}")
     p = subprocess.run(args, capture_output=True)
     out = p.stdout.decode("utf-8")
     if p.returncode:
@@ -57,9 +42,46 @@ def git(*argv: str) -> str:
             print(out)
         if p.stderr:
             print(p.stderr.decode("utf-8"))
-        cmd = " ".join(args)
         die(error, f"Git command '{cmd}' failed", 1)
-    return out
+    return out.strip()
+
+
+def clone_registry() -> str:
+    """Clone the registry."""
+    info("Cloning registry")
+    path = tempfile.mkdtemp()
+    git("clone", env.REGISTRY, path, root=None)
+    info("Cloned registry")
+    return path
+
+
+def get_versions(registry: str) -> Dict[str, str]:
+    """Get a mapping of version -> sha for each version in the registry."""
+    with open(os.path.join(env.REPO_DIR, "Project.toml")) as f:
+        project = toml.load(f)
+    uuid = project["uuid"]
+    with open(os.path.join(registry, "Registry.toml")) as f:
+        reg = toml.load(f)
+    path = reg["packages"][uuid]["path"]
+    with open(os.path.join(registry, path, "Versions.toml")) as f:
+        versions = toml.load(f)
+    return {v: versions[v]["git-tree-sha1"] for v in versions}
+
+
+def rollback(repo: str) -> None:
+    """Roll back a repo to a commit 3 days ago."""
+    # https://stackoverflow.com/a/20801396
+    sha = git("rev-list", "-n", "1", "--before", "3 days ago", "master", root=repo)
+    git("checkout", sha, root=repo)
+
+
+def get_new_versions() -> Dict[str, str]:
+    """Collect all new versions of the package."""
+    path = clone_registry()
+    current = get_versions(path)
+    rollback(path)
+    old = get_versions(path)
+    return {v: sha for v, sha in current.items() if v not in old}
 
 
 def release_exists(version: str) -> bool:
@@ -72,6 +94,15 @@ def release_exists(version: str) -> bool:
         if rel.tag_name == version:
             return True
     return False
+
+
+def commit_from_tree(tree: str) -> Optional[str]:
+    """Get the commit SHA that corresponds to a tree SHA."""
+    lines = git("log", "--format=raw").splitlines()
+    for i, line in enumerate(lines):
+        if line == f"tree {tree}" and lines[i - 1].startswith("commit "):
+            return lines[i - 1].split()[1]
+    return None
 
 
 def tag_exists(version: str) -> bool:
