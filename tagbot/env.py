532fa50c115c8fdff052b899ec9b6a35a775225d
Chris de Graaf <chrisadegraaf@gmail.com>
2019-11-10 21:21:09 +0700

    Set defaults in code instead of action configuration
    Renamed the GitHub inputs back to `github-*` because in one run I saw
    that the `INPUT_GITHUB-*` variables actually were passed through, but
    it's really inconsistent. I have no idea what's going on, really.

--- tagbot/env.py
+++ tagbot/env.py
@@ -1,13 +1,9 @@
 import os
 
-if "GITHUB_ACTION" in os.environ:
-    GITHUB_SITE = os.getenv("INPUT_GH-SITE", "")
-    GITHUB_API = os.getenv("INPUT_GH-API", "")
-else:  # For dev (can't set environment variables with dashes).
-    GITHUB_SITE = os.getenv("GH_SITE", "")
-    GITHUB_API = os.getenv("GH_API", "")
+GITHUB_SITE = os.getenv("INPUT_GITHUB-SITE", "https://github.com")
+GITHUB_API = os.getenv("INPUT_GITHUB-API", "https://api.github.com")
 GPG_KEY = os.getenv("INPUT_GPG-KEY", "")
-REGISTRY = os.getenv("INPUT_REGISTRY", "")
+REGISTRY = os.getenv("INPUT_REGISTRY", "JuliaRegistries/General")
 REPO = os.getenv("GITHUB_REPOSITORY", "")
 REPO_DIR = os.getenv("GITHUB_WORKSPACE", "")
 TOKEN = os.getenv("INPUT_TOKEN", "")
