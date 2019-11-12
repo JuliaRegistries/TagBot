05a26f52dcae521c3129ffb1de10d4f304f8ccfc
Chris de Graaf <chrisadegraaf@gmail.com>
2019-11-12 20:36:00 +0700

    Try to properly set environment values with defaults
    GitHub Actions seems to have a bug. This might work around it.

--- tagbot/env.py
+++ tagbot/env.py
@@ -1,9 +1,14 @@
 import os
 
-GITHUB_SITE = os.getenv("INPUT_GITHUB-SITE", "https://github.com")
-GITHUB_API = os.getenv("INPUT_GITHUB-API", "https://api.github.com")
+# Duplicating the default value for these required ones:
+# https://github.community/t5/GitHub-Actions/Actions-input-parameters-are-not-passed-to-images-pulled-from-a/m-p/33883
+GITHUB_SITE = os.getenv("INPUT_GITHUB-SITE") or "https://github.com"
+GITHUB_API = os.getenv("INPUT_GITHUB-API") or "https://api.github.com"
+
+# This one has no good default, so I think it will be broken until a GitHub fix.
 GPG_KEY = os.getenv("INPUT_GPG-KEY", "")
-REGISTRY = os.getenv("INPUT_REGISTRY", "JuliaRegistries/General")
+
+REGISTRY = os.getenv("INPUT_REGISTRY", "")
 REPO = os.getenv("GITHUB_REPOSITORY", "")
 REPO_DIR = os.getenv("GITHUB_WORKSPACE", "")
 TOKEN = os.getenv("INPUT_TOKEN", "")
