728789b3128eff936224b8f7dbffd251f367cc02
Chris de Graaf <chrisadegraaf@gmail.com>
2020-02-08 19:55:45 +0700

    Update the deprecation notice

--- github/deprecation.go
+++ github/deprecation.go
@@ -15,8 +15,8 @@ const DeprecationNotice = `
 ### IMPORTANT: TagBot as a GitHub App is deprecated
 
 [TagBot as a GitHub App is deprecated](https://github.com/apps/julia-tagbot); you should now migrate to [TagBot as a GitHub Action](https://github.com/marketplace/actions/julia-tagbot).
-Your repository should have received a pull request doing this for you, but if you did not receive this pull request, then see [here](https://github.com/JuliaRegistries/TagBot#setup) for instructions.
-For more information about this deprecation, please see [this Discourse post](TODO DISCOURSE LINK).
+Your repository should have received a pull request doing this for you, but if you did not receive this pull request, then see [here](https://github.com/marketplace/actions/julia-tagbot#setup) for instructions.
+For more information about this deprecation, please see [this Discourse post](https://discourse.julialang.org/t/ann-the-tagbot-github-app-is-deprecated-in-favour-of-the-tagbot-github-action/34344).
 `
 
 // IsActionEnabled checks whether or not TagBot is enabled as a GitHub Action.
