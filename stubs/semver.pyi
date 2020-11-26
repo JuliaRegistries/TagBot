0242da8dffc3943c66d40e438d442eb23a477cea
Chris de Graaf <me@cdg.dev>
2020-11-26 10:40:29 -0600

    New automated release process (#183)

--- stubs/semver.pyi
+++ stubs/semver.pyi
@@ -1,9 +1,14 @@
 from __future__ import annotations
+from typing import Optional
 
 class VersionInfo:
-    prerelease: str
-    build: str
+    major: str
+    minor: str
+    patch: str
+    prerelease: Optional[str]
+    build: Optional[str]
     def __init__(self, major: int) -> None: ...
     def __lt__(self, other: VersionInfo) -> bool: ...
     @staticmethod
     def parse(version: str) -> VersionInfo: ...
+    def next_version(self, bump: str) -> VersionInfo: ...
