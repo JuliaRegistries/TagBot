6d87a3414ef2db0ca7723ba56691e3eebcd07906
Chris de Graaf <me@cdg.dev>
2021-03-26 00:09:47 -0500

    Pedantry

--- bin/docker_build.jl
+++ bin/docker_build.jl
@@ -1,7 +1,7 @@
-ENV["PYTHON"] = Sys.which("python")
 using Pkg: Pkg, Registry, TOML
+
+ENV["PYTHON"] = Sys.which("python")
 Pkg.instantiate()
 Registry.rm("General")
-
 python_deps = collect(TOML.parsefile("Project.toml")["python"])
 foreach(dep -> run(`pip install $(dep.first)==$(dep.second)`), python_deps)
