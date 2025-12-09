2e2033dae46b94163047b701cdd3a99bbc7538df
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-12-09 15:19:50 +0530

    Update repo.py

--- tagbot/action/repo.py
+++ tagbot/action/repo.py
@@ -131,7 +131,7 @@ class Repo:
         except (GithubException, RequestException) as exc:
             # This is an awful hack to let me avoid properly fixing the tests...
             if "pytest" in sys.modules:
-                logger.warning("'awful hack' in use")
+                logger.warning("'awful hack' in use", exc_info=exc)
                 self._registry = self._gh.get_repo(registry, lazy=True)
                 self._clone_registry = False
             else:
