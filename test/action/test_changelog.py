aac0d684b1a8f339e2d08125f6660a014d046cee
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-12-23 10:49:13 +0530

    test_changelog - 2

--- test/action/test_changelog.py
+++ test/action/test_changelog.py
@@ -268,6 +268,7 @@ def test_render():
     expected = """
     ## PkgName v1.2.3
 
+    
     [Diff since v1.2.2](https://github.com/Me/PkgName.jl/compare/v1.2.2...v1.2.3)
 
     Custom release notes
@@ -314,6 +315,7 @@ def test_render_backport():
     expected = """
     ## PkgName v1.2.3
 
+    
     [Diff since v1.2.2](https://github.com/Me/PkgName.jl/compare/v1.2.2...v1.2.3)
 
     Custom release notes
